524b1f64c45d484fc98bf62fb8646db5e88b1370
jemtz <jemtz.deleon@hotmail.com>
2025-08-20 15:52:49 -0700

    another test fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ClockInOut/', // This must match your repo name!
+  base: '/ClockInOut/', // <-- Your repo name
 });
